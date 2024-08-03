80ed4f98cea5cb6e4c0cf9ce5d71bbb3f75ec0c5
yunzhewong <84904965+yunzhewong@users.noreply.github.com>
2024-08-03 20:21:55 +1000

    Initial commit

---
+++ ELEN90064_Project_Parameters.m
@@ -0,0 +1,55 @@
+clear
+
+%% DEFINE THE PLANT INITIAL CONDITIONS
+
+% Initial link angle [radians]
+theta_l_init = deg2rad(0);
+% Initial link velocity [radians/second]
+thetadot_l_init = deg2rad(0);
+% Initial motor angle [radians]
+theta_m_init = deg2rad(0);
+% Initial motor velocity [radians/second]
+thetadot_m_init = deg2rad(0);
+% Initial motor current [Amps]
+Ia_init = 0;
+
+% Initial conditions stacked as a 5x1 vector
+init = [theta_m_init; theta_l_init; thetadot_m_init; thetadot_l_init; Ia_init];
+
+%% DEFINE THE SENSOR PARAMETERS
+
+% FOR VISION:
+% > The frames per second [fps]
+% > Allowed values: 25, 50, 100
+vision_fps = 25;
+
+% FOR ABSOLUTE ENCODER
+% > Number of counts per revolution
+absolute_encoder_counts_per_revolution = 128;
+
+% FOR POTENTIOMETER
+% > Percentage linearity [percent]
+potentiometer_linearity_percent = 20;
+
+% FOR TACHOMETER
+% > Number of counts per revolution
+tachometer_counts_per_revolution = 2048;
+% > Frequency of the velocity estimates
+% > Allowed values: 10, 25, 50, 100
+tachometer_output_frequency = 50;
+
+% FOR GYROSCOPE
+% > Variance of the noise that drives the drift process [rad/s^2]
+gyro_sigma_drift = deg2rad(5);
+
+
+%% Link Parameters
+m = 0.1;
+l = 0.1;
+g = 9.81;
+
+%% Wave Parameters
+wave_amplitude = deg2rad(15);
+wave_offset = deg2rad(30);
+wave_period = 6;
+wave_frequency = wave_period / (2 * pi);

2866ab3c85e80e344f1d48b8213b52d05a6aa343
yunzhewong <84904965+yunzhewong@users.noreply.github.com>
2024-08-06 14:12:31 +1000

    trying to change my linearised system

--- linearised_incremental.m
+++ linearised_incremental.m
@@ -1,17 +1,22 @@
-K_s = 1.6;
-J_m = 1; % Unknown
+K_s = 1.8;
+J_m = 7.5e-6; 
 J_t = 0.001;
 
 A = [0 0 1 0 0;
      0 0 0 1 0;
      -K_s/J_m K_s/J_m 0 0 0;
-     K_s/J_t -K_s/J_t-sqrt(3) * m*g*l/(2 * J_t) 0 0 0;
+     K_s/J_t -K_s/J_t 0 0 0;
      0 0 -K_m/L_a 0 -R/L_a];
 
-B = [0; 0; 1/J_m; 0; 1/L_a];
+B = [0; 0; K_m/(R * J_m); 0; 1/L_a];
 
 C = [0 1 0 0 0];
 D = 0;
+
+A
+B
+C
+D
 
 [b, a] = ss2tf(A, B, C, D);
 

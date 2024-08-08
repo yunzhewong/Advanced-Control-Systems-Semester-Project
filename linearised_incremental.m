a7197c0960de1ba3b3d87317e7e7eeb47b733632
yunzhewong <84904965+yunzhewong@users.noreply.github.com>
2024-08-08 19:29:08 +1000

    specification checking

--- linearised_incremental.m
+++ linearised_incremental.m
@@ -1,11 +1,19 @@
-K_s = 1.8;
+close all
+
 J_m = 7.5e-6; 
 J_t = 0.001;
+x2_bar = pi / 6;
+T_bar = m * g * l / 2;
+V_bar = ((T_bar / K_m) + I_noload) * R;
+
+x1_bar = deg2rad(31.78);
+K_s = T_bar / (x1_bar - x2_bar);
+K_s
 
 A = [0 0 1 0 0;
      0 0 0 1 0;
      -K_s/J_m K_s/J_m 0 0 0;
-     K_s/J_t -K_s/J_t 0 0 0;
+     K_s/J_t -K_s/J_t-(sqrt(3)/2)*m*g*l/J_t 0 0 0;
      0 0 -K_m/L_a 0 -R/L_a];
 
 B = [0; 0; K_m/(R * J_m); 0; 1/L_a];
@@ -22,4 +30,22 @@ D
 
 sys = tf(b, a);
 
+s = tf('s')
+
+w_c = 3;
+tz = 10 / w_c
+tp = 1 / (10 * w_c)
+C = (tz*s + 1)^2/(tp*s + 1)^2
+zpksys = zpk(sys)
+
+figure
 bode(sys)
+
+figure
+bode(C)
+
+figure
+bode(sys * C)
+
+figure
+rlocus(sys * C)
---
+++ specifications.m
@@ -0,0 +1,20 @@
+STEADY_STATE_ERROR_DEGREES = 2;
+OVERSHOOT_DEGREES = 5;
+SETTLING_TIME_SECONDS = 2; % 2% settling time
+
+LOW_REFERENCE_DEGREES = 15;
+HIGH_REFERENCE_DEGREES = 45;
+
+overshoot_percent = OVERSHOOT_DEGREES / (HIGH_REFERENCE_DEGREES - LOW_REFERENCE_DEGREES)
+
+
+syms zeta
+zeta = -1 * solve(log(overshoot_percent) == -pi * zeta / (sqrt(1 - zeta^2)))
+
+syms wn
+wn = solve(-SETTLING_TIME_SECONDS * zeta * wn == log((2 / 100) * sqrt(1 - zeta^2)))
+
+wc = wn * sqrt(sqrt(1 + 4 * zeta^4) - 2 * zeta^2)
+Mf_rad = atan((2 * zeta) / (sqrt(sqrt(1 + 4 * zeta^4) - 2 * zeta^2)));
+
+Mf_deg = rad2deg(Mf_rad)

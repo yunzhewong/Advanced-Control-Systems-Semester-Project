b3c433de41bb7309598e86674c9065cd4fd9377b
yunzhewong <84904965+yunzhewong@users.noreply.github.com>
2024-08-10 15:26:59 +1000

    maybe tuned

--- linearised_incremental.m
+++ linearised_incremental.m
@@ -26,7 +26,7 @@ sys = tf(sys_b, sys_a);
 
 w_c = 20;
 
-controller = zpk([-10+460i -10-460i -3-9i -3+9i -4], [-5 0 -10000 -10000 -10000], 1e7);
+controller = zpk([-10+460i -10-460i -3-6i -3+6i -4], [-5 0 -10000 -10000 -10000], 1e7);
 [c_b, c_a] = tfdata(controller,'v');
 
 figure

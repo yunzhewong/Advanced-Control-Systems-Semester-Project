598d796e185116f9907eb624a0d93e312f7a67a4
yunzhewong <84904965+yunzhewong@users.noreply.github.com>
2024-09-08 23:15:06 +1000

    continuous time reduced order observer

--- Phase 2/observer_design.m
+++ Phase 2/observer_design.m
@@ -1,8 +1,8 @@
-
 C_o = [1 0 0 0;
        0 1 0 0];
 
-eig(A)
+% Continuous Time - Full State Observer Design
+eig(A);
 
 closest_pole = -100;
 
@@ -11,4 +11,18 @@ J_observer_transpose = place(A', C_o', observer_poles);
 J_observer = J_observer_transpose';
 
 observer_matrix = A - J_observer*C_o;
-eig(observer_matrix)
+eig(observer_matrix);
+
+% Discrete Time - Reduced Order Observer
+disc_a = sys_disc.A;
+disc_b = sys_disc.B;
+
+A_aa = A(1:2, 1:2);
+A_ab = A(1:2, 3:4);
+A_ba = A(3:4, 1:2);
+A_bb = A(3:4, 3:4);
+
+B_a = B(1:2, :);
+B_b = B(3:4, :);
+
+L_r = place(A_bb', A_ab', [closest_pole, closest_pole+1])'

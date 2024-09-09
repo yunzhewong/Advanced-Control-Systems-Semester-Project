be8b71efd57f901980c0ede29f8bbd01e843527c
yunzhewong <84904965+yunzhewong@users.noreply.github.com>
2024-09-09 18:44:45 +1000

    discrete reduced order observer

--- Phase 2/observer_design.m
+++ Phase 2/observer_design.m
@@ -25,4 +25,15 @@ A_bb = A(3:4, 3:4);
 B_a = B(1:2, :);
 B_b = B(3:4, :);
 
-L_r = place(A_bb', A_ab', [closest_pole, closest_pole+1])'
+L_r = place(A_bb', A_ab', [closest_pole, closest_pole+1])';
+
+A_aa_disc = disc_a(1:2, 1:2);
+A_ab_disc = disc_a(1:2, 3:4);
+A_ba_disc = disc_a(3:4, 1:2);
+A_bb_disc = disc_a(3:4, 3:4);
+
+B_a_disc = disc_b(1:2, :);
+B_b_disc = disc_b(3:4, :);
+
+disc_poles = exp([closest_pole, closest_pole+1] * min_sampling_time);
+L_r_disc = place(A_bb_disc', A_ab_disc', disc_poles)';

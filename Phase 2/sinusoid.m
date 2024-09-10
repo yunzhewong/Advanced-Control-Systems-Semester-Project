7ed636b7a7877e52d42978a9628a0c658c04fccb
yunzhewong <84904965+yunzhewong@users.noreply.github.com>
2024-09-10 14:19:29 +1000

    working sinusoid following

--- Phase 2/sinusoid.m
+++ Phase 2/sinusoid.m
@@ -1,18 +1,28 @@
 T = 4;
 w = 2 * pi / T;
 
-A_s = [0 1; -w^2 0];
+A_s = [0 w; -w 0];
 
 C_sin = [0 1 0 0;
-         0 0 0 1];
+         0 0 0 0];
 err_dyna = [0 0 0 1; 0 0 0 0];
 
-A_sin_aug = [0 C;zeros(4,1) A]
-B_sin_aug = [zeros(1, 1); B]
+A_sin_aug = [A_s zeros(2, 1) C_sin; zeros(1,2) 0 C; zeros(4,3) A]
+B_sin_aug = [zeros(3, 1); B]
 
-K_sin = place(A_sin_aug, B_sin_aug, [-3+2i -3-2i -25+400i -25-400i -50])
+K_sin = place(A_sin_aug, B_sin_aug, [-2.5+8i -2.5-8i -25 -26 -50 -60 -70])
 K_sin_s = K_sin(1:2)
-K_sin_int = K_sin(1)
-K_sin_x = K_sin(2:5)
+K_sin_int = K_sin(3)
+K_sin_x = K_sin(4:7)
+
+% A_sin_aug = [A_s C_sin; zeros(4,2) A]
+% B_sin_aug = [zeros(2, 1); B]
+% 
+% K_sin = place(A_sin_aug, B_sin_aug, [-1 -2 -25 -26 -50 -60])
+% K_sin_s = K_sin(1:2)
+% % K_sin_int = K_sin(3)
+% K_sin_x = K_sin(3:6)
+
+
 
 % eig(A_sin_aug - B_sin_aug*K_sin)
